dae03cfae814b56a4b04b3af96412d98fc4ef71d
Piyali8991 <piyalinath4002@gmail.com>
2025-04-23 13:51:16 +0530

    Updates

---
+++ precache-manifest.d7192cfa6bd2d3bfec1968490082f955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d3ca26587314b5b20ed18babb7500d",
+    "url": "/Piyali8991/Your_Own_Time_Capsule.git/index.html"
+  },
+  {
+    "revision": "eb656afe4d9246307776",
+    "url": "/Piyali8991/Your_Own_Time_Capsule.git/static/css/main.92266101.chunk.css"
+  },
+  {
+    "revision": "5d512f6d16c1cd5ca5e2",
+    "url": "/Piyali8991/Your_Own_Time_Capsule.git/static/js/2.9f5a8f8f.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/Piyali8991/Your_Own_Time_Capsule.git/static/js/2.9f5a8f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb656afe4d9246307776",
+    "url": "/Piyali8991/Your_Own_Time_Capsule.git/static/js/main.4b99af2d.chunk.js"
+  },
+  {
+    "revision": "4144a666a5b6a7eb7392",
+    "url": "/Piyali8991/Your_Own_Time_Capsule.git/static/js/runtime-main.b548b454.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/Piyali8991/Your_Own_Time_Capsule.git/static/media/memories.5c0c63fb.png"
+  }
+]);
